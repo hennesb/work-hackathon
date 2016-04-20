9faf0df033d5b75280db6a91fcbbdd474b3b69e2
hennesb <hennesb@gmail.com>
2016-04-20 10:25:31 +0100

    Putting in unit test structure

---
+++ src/test/java/ie/services/MyUnitTest.java
@@ -0,0 +1,12 @@
+package ie.services;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+    public void stubTest() {
+        assertEquals(0, 0);
+
+    }
+}
