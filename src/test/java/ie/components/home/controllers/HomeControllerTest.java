c90d72b53c0267a31e22c74c7db6416908f67176
hennesb <hennesb@gmail.com>
2016-06-23 22:52:22 +0100

    Cleaning up code

--- src/test/java/ie/components/home/controllers/HomeControllerTest.java
+++ src/test/java/ie/components/home/controllers/HomeControllerTest.java
@@ -3,7 +3,6 @@
  */
 package ie.components.home.controllers;
 
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -13,21 +12,18 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import ie.components.configuration.AppConfiguration;
-import ie.components.configuration.AppInitializer;
 
 /**
  * @author hennesb
